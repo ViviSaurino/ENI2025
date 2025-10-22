183be4626160ea74e7fd926c2da4b40ad7091c55
Vivi Saurino <a20098917@pucp.edu.pe>
2025-10-22 16:56:18 -0500

    Update auth_google.py

--- auth_google.py
+++ auth_google.py
@@ -115,7 +115,7 @@ def google_login(
               height:100%;
               display:flex;
               align-items:center;       /* alinea verticalmente las dos columnas */
-              gap: 30px !important;      /* ⬅️ SEPARACIÓN ENTRE COLUMNAS (ajusta 4px, 6px, 8px, etc.) */
+              gap: 0px !important;      /* ⬅️ SEPARACIÓN ENTRE COLUMNAS (ajusta 4px, 6px, 8px, etc.) */
             }}
 
             /* 👇 Control maestro del ancho (VENIDOS + píldora + botón) y separaciones */
@@ -350,6 +350,7 @@ def google_login(
 def logout():
     st.session_state.pop("user", None)
     _safe_rerun()
+
 
 
 
