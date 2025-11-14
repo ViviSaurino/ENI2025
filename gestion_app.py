ef473290d5ad999ddf0dbb9e0c4b6ed01c31cf61
Vivi Saurino <a20098917@pucp.edu.pe>
2025-11-14 18:01:35 -0500

    Update gestion_app.py

--- gestion_app.py
+++ gestion_app.py
@@ -177,7 +177,7 @@ def check_app_password() -> bool:
     st.markdown("<div style='margin-top:7vh;'></div>", unsafe_allow_html=True)
 
     # Columnas generales con espaciador a la izquierda
-    space_col, col1, col2 = st.columns([0.15, 0.55, 0.35])
+    space_col, col1, col2 = st.columns([0.20, 0.55, 0.35])
         
     # Columna izquierda: título + subcolumna
     with col1:
