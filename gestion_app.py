89aaec2bb20edb4035d5fe5079bee98c9d78c78a
Vivi Saurino <a20098917@pucp.edu.pe>
2025-11-14 16:55:58 -0500

    Update gestion_app.py

--- gestion_app.py
+++ gestion_app.py
@@ -125,7 +125,7 @@ def check_app_password() -> bool:
       }
       .eni-hero-pill{
         display:inline-block;
-        padding:10px 50px;
+        padding:10px 53px;
         border-radius:12px;
         background-color:#E0ECFF;
         color:#2B3A67;
