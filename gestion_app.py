7932442ece382eafac1fcfd1497020ffa857185b
Vivi Saurino <a20098917@pucp.edu.pe>
2025-11-14 17:23:04 -0500

    Update gestion_app.py

--- gestion_app.py
+++ gestion_app.py
@@ -166,7 +166,7 @@ def check_app_password() -> bool:
     """, unsafe_allow_html=True)
 
     # Margen superior sólo en la pantalla de login
-    st.markdown("<div style='margin-top:15vh;'></div>", unsafe_allow_html=True)
+    st.markdown("<div style='margin-top:10vh;'></div>", unsafe_allow_html=True)
 
     # Columnas generales con espaciador a la izquierda
     space_col, col1, col2 = st.columns([0.15, 0.55, 0.35])
