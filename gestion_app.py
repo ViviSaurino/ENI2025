aca151385f7273e0104b758a7ecffa7e3297d63d
Vivi Saurino <a20098917@pucp.edu.pe>
2025-11-16 18:01:07 -0500

    Update gestion_app.py

--- gestion_app.py
+++ gestion_app.py
@@ -322,8 +322,8 @@ st.markdown(
 
   /* Reducir al mínimo el espacio horizontal entre columnas (lila/blanco y tarjetas) */
   div[data-testid="stHorizontalBlock"]{
-    gap: 0.15rem !important;        /* antes ~1rem, ahora casi nada */
-    column-gap: 0.15rem !important;
+    gap: 0.2rem !important;        /* antes ~1rem, ahora casi nada */
+    column-gap: 0.2rem !important;
   }
 
 </style>
