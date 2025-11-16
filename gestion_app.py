dadc8569df213b296924c5976ed7b971a74ec63f
Vivi Saurino <a20098917@pucp.edu.pe>
2025-11-16 16:30:04 -0500

    Update gestion_app.py

--- gestion_app.py
+++ gestion_app.py
@@ -100,7 +100,7 @@ st.markdown(
     display:flex;
     align-items:center;
     justify-content:flex-end;
-    margin:0 0px 18px 0px;   /* top  right  bottom  left */
+    margin:0 -70px 18px -70px;   /* top  right  bottom  left */
     box-shadow:0 12px 26px rgba(15,23,42,0.10);
   }
   .eni-main-topbar-title{
