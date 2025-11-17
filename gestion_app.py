9287e14421426b0eef88715c1d25084117da42c5
Vivi Saurino <a20098917@pucp.edu.pe>
2025-11-17 00:51:02 -0500

    Update gestion_app.py

--- gestion_app.py
+++ gestion_app.py
@@ -249,7 +249,7 @@ st.markdown(
   }
 
   html body [data-testid="stAppViewContainer"] .main .block-container{
-    padding-top:1.8rem !important;
+    padding-top:2.5rem !important;
     margin-top:0rem !important;
     background:transparent;
   }
