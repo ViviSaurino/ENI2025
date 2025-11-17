d6ce5fd0599138a2694732243477c757ac9609e8
Vivi Saurino <a20098917@pucp.edu.pe>
2025-11-17 00:01:18 -0500

    Update gestion_app.py

--- gestion_app.py
+++ gestion_app.py
@@ -336,7 +336,7 @@ st.markdown(
     display:flex;
     justify-content:space-between;
     align-items:center;
-    margin-top:-130px;
+    margin-top:-140px;
   }
   .eni-quick-card-wide-nt .eni-quick-card-title{
     font-size:15px;
