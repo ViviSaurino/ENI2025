ea5e2fe7d38c42b7dfb9827336f5e845d00f6d73
Vivi Saurino <a20098917@pucp.edu.pe>
2026-01-14 21:54:58 -0500

    Update gestion_app.py

--- gestion_app.py
+++ gestion_app.py
@@ -91,7 +91,7 @@ st.markdown(
     position:fixed;
     top:0; left:0; right:0;
     height:120px;                 /* “arriba un celeste más claro” */
-    background:#e0e6f6;           /* celeste claro */
+    background:#e4e3e9;           /* celeste claro */
     z-index:0;
   }
   /* Asegura que el contenido quede encima de la franja */
